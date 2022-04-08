5bd460e9b824b927cf4a2b2bd3dbc79cbb08a723
ALOKPANDEY05498 <alokpandey2095.com>
2022-04-08 18:09:38 +0000

    commited-code

---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import LogoutButton from '../LogoutButton'
+import Header from '../Header'
+
+const Home = () => (
+  <>
+    <Header />
+    <div className="Home">
+      <h1>Home Route</h1>
+      <LogoutButton />
+    </div>
+  </>
+)
+
+export default Home
